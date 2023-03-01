aaa3b5b578e3926ce9c1d0ec7ddef8304b2f9f6c
Danylo <evg829@gmail.com>
2023-03-01 20:52:59 +0200

    Added rds vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,18 @@ variable "ssh_pub_key_path" {
   type = string
   description = "Path for public ssh key"
 }
+
+variable "rds_name" {
+  type = string
+  description = "The name of RDS"
+}
+
+variable "rds_username" {
+  type = string
+  description = "The name of RDS user"
+}
+
+variable "rds_password" {
+  type = string
+  description = "The password of RDS user"
+}
