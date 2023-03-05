a4d9513ef78197ea73ec00bd39d5d91dd7aa8ee6
Danylo <evg829@gmail.com>
2023-03-05 23:38:52 +0200

    Removed NAT gateways

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -32,19 +32,6 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
 
-# The NAT Gateway
-resource "aws_nat_gateway" "main" {
-  count         = length(var.availability_zones)
-  allocation_id = aws_eip.main[count.index].id
-  subnet_id     = aws_subnet.public[count.index].id
-}
-
-# The Elastic IP address for the NAT Gateway
-resource "aws_eip" "main" {
-  count = length(var.availability_zones)
-  vpc   = true
-}
-
 # The public routing table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -55,29 +42,11 @@ resource "aws_route_table" "public" {
   }
 }
 
-# The private routing table
-resource "aws_route_table" "private" {
-  count  = length(var.availability_zones)
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.main[count.index].id
-  }
-}
-
 # Public routing table
 resource "aws_route_table_association" "public" {
   count          = length(var.availability_zones)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
-}
-
-# Private routing table
-resource "aws_route_table_association" "private" {
-  count          = length(var.availability_zones)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private[count.index].id
 }
 
 # Security group allowing incoming SSH traffic and http
