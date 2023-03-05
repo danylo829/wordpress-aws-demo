3e912109bf1241489154658f4503bdac734f790b
Danylo <evg829@gmail.com>
2023-03-05 23:37:54 +0200

    Code cleanup

--- terraform/bucket/outputs.tf
+++ terraform/bucket/outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_name" {
-  value = aws_s3_bucket.state_bucket.bucket
+  value       = aws_s3_bucket.state_bucket.bucket
   description = "The name of state bucket (used in backend.conf)"
 }
 
 output "bucket_region" {
-  value = aws_s3_bucket.state_bucket.region
+  value       = aws_s3_bucket.state_bucket.region
   description = "The region of state bucket (used in backend.conf)"
 }
